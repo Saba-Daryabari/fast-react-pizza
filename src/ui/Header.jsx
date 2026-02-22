b23eadfa91d9a352fe2cea4276bacf5e165904e3
Saba Daryabari <Sabadbdb@gmail.com>
2026-02-22 13:48:51 +0330

    add custome font family.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,9 +4,8 @@ import UserName from "../Features/user/UserName";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 p-4 sm:px-6">
+    <header className="font-sans flex items-center justify-between bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 p-4 sm:px-6 ">
       <Link to="/">Fast React Pizza Company</Link>
-
       <SearchOrder />
       <UserName />
     </header>
